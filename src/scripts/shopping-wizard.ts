1a11394c6db02565ee022655482b4813d5a00947
Jorge Alonso <jalonsomerchan@gmail.com>
2026-06-09 12:38:22 +0200

    Load shopping list management enhancers

--- src/scripts/shopping-wizard.ts
+++ src/scripts/shopping-wizard.ts
@@ -1,5 +1,7 @@
 import { useShoppingActionTranslations } from '../i18n/shopping-actions';
 import './shopping-alexa-integration';
+import './shopping-list-actions';
+import '../styles/shopping-list-actions.css';
 
 const wizard = document.querySelector<HTMLElement>('[data-shopping-wizard]');
 
